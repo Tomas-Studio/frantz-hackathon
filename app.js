53a93bd3e17cc705cd3656c3f6bd95a0b0ca0593
Olakunle Olatunji <tjomas20@gmail.com>
2023-11-26 00:51:21 +0100

    feat: styled range and wrote function to control range

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import './app.css'
 
-const toggleGuide = document.querySelector(".btn-icon-light-2");
+// get elements 
+const toggleGuide = document.querySelector(".btn-icon-light-2")
+const range = document.querySelector('#range')
 
+
+// control element & styles
 toggleGuide.addEventListener("click", () => {
   toggleGuide.dataset.toggle === 'closed' 
   ? toggleGuide.dataset.toggle = 'open'
   : toggleGuide.dataset.toggle = 'closed' 
 })
+
+range.addEventListener('input', (event)=> {
+  let t = event.target
+  
+  const min = t.min; 
+  const max = t.max;  
+  const val = t.value;  
+  
+  t.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'; 
+})
+
+range.style.backgroundSize = (range.value - range.min) * 100 / (range.max - range.min) + '% 100%';
