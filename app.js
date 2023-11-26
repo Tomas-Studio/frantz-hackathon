f13c09f5c04c533001357dc5efce5711f9af9b87
Olakunle Olatunji <tjomas20@gmail.com>
2023-11-26 07:17:58 +0100

    feat: added animation to check list review

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const checkList = document.querySelector('.check-list')
 toggleGuide.addEventListener("click", () => {
   if(toggleGuide.dataset.toggle === 'closed'){
     toggleGuide.dataset.toggle = 'open'
-    checkList.style.height = 'auto'
+    checkList.style.height = `${checkList.scrollHeight}px`
   } else {
     toggleGuide.dataset.toggle = 'closed' 
     checkList.style.height = '0px'
@@ -30,4 +30,3 @@ range.addEventListener('input', (event)=> {
 
 // initialize values
 range.style.backgroundSize = (range.value - range.min) * 100 / (range.max - range.min) + '% 100%';
-checkList.style.height = '0px'
